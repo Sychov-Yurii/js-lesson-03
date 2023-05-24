944dbea5bd19b23e9b5956ec55f5d1675a6d16a0
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-24 23:54:01 +0200

    1

---
+++ script.js
@@ -0,0 +1,12 @@
+// ================= work 1 ================= //
+
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     tags: ["premium", "promoted", "top"],
+//     price: 2153,  
+//     public: true,
+//     rating: 4,
+//   };
+
+  // ================= work 2 ================= //
